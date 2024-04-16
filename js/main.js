b7a527c7bfeda76f1e94896c6e37340462f1945b
fbadqui <fadia.badavid@emtelco.com.co>
2024-04-16 14:27:03 -0500

    JavaScript Primera Pre-entrega

--- js/main.js
+++ js/main.js
@@ -2,48 +2,52 @@
 
 const TipoVehiculo = parseInt(prompt("Qué tipo de vehículo quieres? \n\n1.Carro\n2.Moto"));
 if (TipoVehiculo == 1) {
-    let ValorCarro = parseFloat(prompt("Cuánto es el valor aproximado del carro?\nIngrese el valor en pesos Colombianos"));
-    let CuotaInicialCarro = parseFloat(prompt("Ingresa tu cuota inicial\nIngrese el valor en pesos Colombianos"));
-    let MesesCarro = parseInt(prompt("¿A cuántos meses quieres pagar? \n\n.12\n.24\n36"));
-    CalcularCuotaCarro(ValorCarro, CuotaInicialCarro, MesesCarro);
-    ElegirTipoVehiculo(1, ValorCarro, CuotaInicialCarro, MesesCarro);
+    let Valor = parseFloat(prompt("Cuánto es el valor aproximado del carro?\nIngrese el valor en pesos Colombianos"));
+    let CuotaInicial = parseFloat(prompt("Ingresa tu cuota inicial\nIngrese el valor en pesos Colombianos"));
+    let Meses = parseInt(prompt("¿A cuántos meses quieres pagar? \n\n.12\n.24\n.36"));
+    CalcularCuotaCarro(Valor, CuotaInicial, Meses);
+    ElegirTipoVehiculo(1, Valor, CuotaInicial, Meses);
 } else if (TipoVehiculo == 2) {
-    let ValorMoto = parseFloat(prompt("Cuánto es el valor aproximado de la moto?\nIngrese el valor en pesos Colombianos"));
-    let CuotaInicialMoto = parseFloat(prompt("Ingresa tu cuota inicial\nIngrese el valor en pesos Colombianos"));
-    let MesesMoto = parseFloat(prompt("¿A cuántos meses quieres pagar? \n\n1).12\n2).24\n3).36"));
-    ElegirTipoVehiculo(2);
+    let Valor = parseFloat(prompt("Cuánto es el valor aproximado de la moto?\nIngrese el valor en pesos Colombianos"));
+    let CuotaInicial = parseFloat(prompt("Ingresa tu cuota inicial\nIngrese el valor en pesos Colombianos"));
+    let Meses = parseFloat(prompt("¿A cuántos meses quieres pagar? \n\n.12\n.24\n.36"));
+    CalcularCuotaCarro(Valor, CuotaInicial, Meses);
+    ElegirTipoVehiculo(2, Valor, CuotaInicial, Meses);
+    
 } else {
     alert("No ingresaste un valor correcto")
 }
 //funcion para calcular cuota carro
-function CalcularCuotaCarro(ValorCarro, CuotaInicialCarro, MesesCarro) {
-    let MontoFinanciado = ValorCarro - CuotaInicialCarro;
-    let CuotaMensual = MontoFinanciado / MesesCarro;
+function CalcularCuotaCarro(Valor, CuotaInicial, Meses) {
+    let MontoFinanciado = Valor - CuotaInicial;
+    let CuotaMensual = MontoFinanciado / Meses;
     return CuotaMensual
 }
 
 //funcion para calcular cuota Moto
-function CalcularCuotaMoto(ValorMoto, CuotaInicialMoto, MesesMoto) {
-    let MontoFinanciado = ValorMoto - CuotaInicialMoto;
-    let CuotaMensual = MontoFinanciado / MesesMoto;
+function CalcularCuotaMoto(Valor, CuotaInicial, Meses) {
+    let MontoFinanciado = Valor - CuotaInicial;
+    let CuotaMensual = MontoFinanciado / Meses;
 
     return CuotaMensual
 }
 
 //funcion de menu inicial
 
-function ElegirTipoVehiculo(TipoVehiculo, ValorCarro, CuotaInicialCarro, MesesCarro) {
+function ElegirTipoVehiculo(TipoVehiculo, Valor, CuotaInicial, Meses) {
     switch (TipoVehiculo) {
         case 1:
 
-            let CuotaMensual = CalcularCuotaCarro(ValorCarro, CuotaInicialCarro, MesesCarro);
+            let CuotaMensual = CalcularCuotaCarro(Valor, CuotaInicial, Meses);
             let ValorPesos = CuotaMensual.toLocaleString('es-CO')
 
             console.log("Cuota mensual aproximada que pagarías: " + ValorPesos + " pesos, El monto aprobado y la tasa dependerán de tu información financiera y nuestro análisis crediticio.");
             break;
         case 2:
-            CalcularCuotaMoto();
-            console.log("tu cuota mensual es de")
+            let CuotaMensual2 = CalcularCuotaMoto(Valor, CuotaInicial, Meses);
+            let ValorPesos2 = CuotaMensual2.toLocaleString('es-CO')
+
+            console.log("Cuota mensual aproximada que pagarías: " + ValorPesos2 + " pesos, El monto aprobado y la tasa dependerán de tu información financiera y nuestro análisis crediticio.");
             break;
         default:
             console.log("opcion no valida")
